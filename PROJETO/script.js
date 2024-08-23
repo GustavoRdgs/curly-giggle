1f7b25fae78becf7e438092b16f8e4536e126189
Gustavo <154286647+GustavoRdgs@users.noreply.github.com>
2024-08-23 12:38:19 -0300

    Add files via upload

---
+++ PROJETO/script.js
@@ -0,0 +1,7 @@
+document.getElementById('abrir-formulario').addEventListener('click', function() {
+    document.getElementById('formulario').style.display = 'block';
+});
+
+document.getElementById('fechar').addEventListener('click', function() {
+    document.getElementById('formulario').style.display = 'none';
+});
